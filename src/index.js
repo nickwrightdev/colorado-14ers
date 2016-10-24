05c598fb7330099ec4b8fff7a2bb999207f0cbe9
Nick Wright <nick@nickwrightdev.com>
2016-10-24 01:25:31 -0700

    feat(random): Add ability to get array of colorado 14ers
    including test coverage
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqueRandomArray = require('unique-random-array');
 var colorado14ers = require('./colorado-14ers.json');
+var getRandomItem = uniqueRandomArray(colorado14ers);
 
 module.exports = {
 	all: colorado14ers,
-	random: uniqueRandomArray(colorado14ers)
+	random: random14er
 };
+
+function random14er(number) {
+	if (number === undefined) {
+		return getRandomItem();
+	}
+
+	var randomItems = [];
+	for (var i = 0; i < number; i++) {
+		randomItems.push(getRandomItem());
+	}
+	return randomItems;
+}
--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var colorado14ers = require('./index');
 
 describe('colorado-14ers', function () {
-   describe('all', function () {
+    describe('all', function () {
       it('should be an array of strings', function () {
           expect(colorado14ers.all).to.satisfy(isArrayOfStrings);
 
@@ -16,12 +16,20 @@ describe('colorado-14ers', function () {
        it('should contain Pikes Peak', function () {
           expect(colorado14ers.all).to.include('Pikes Peak');
        });
-   });
+    });
 
     describe('random', function () {
-       it('should return a random item from all', function () {
+        it('should return a random item from all', function () {
             var randomItem = colorado14ers.random();
            expect(colorado14ers.all).to.include(randomItem);
-       });
+        });
+
+        it('should return an array of random items if passed a number', function () {
+            var randomItems = colorado14ers.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach(function (item) {
+               expect(colorado14ers.all).to.include(item);
+            });
+        });
     });
 });
